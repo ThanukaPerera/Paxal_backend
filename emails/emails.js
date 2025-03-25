ff59e4eb27fae6f5240bb5028e3001a0c89e724d
Kalani Chandrasena <kalanichandrasena99@gmail.com>
2025-03-25 14:34:01 +0530

    Emails to send parcel tracking number

--- emails/emails.js
+++ emails/emails.js
@@ -1,8 +1,8 @@
-const nodemailer = require('nodemailer');
+const nodemailer = require("nodemailer");
 const path = require("path");
 const fs = require("fs");
 
-require('dotenv').config();
+require("dotenv").config();
 
 const sender = process.env.EMAIL;
 
@@ -10,43 +10,79 @@ const sender = process.env.EMAIL;
 const transporter = nodemailer.createTransport({
   service: "gmail",
   host: "smtp.gmail.com",
-    port: 587,
-    secure: false, 
-    auth: {
-      user: sender,
-      pass: process.env.EMAIL_PASS,
-    },
-  });
-
+  port: 587,
+  secure: false,
+  auth: {
+    user: sender,
+    pass: process.env.EMAIL_PASS,
+  },
+});
 
 // GET PASSWORD RESET EMAIL TEMPLATE
 const getResetEmail = (resetCode) => {
-  const resetEmailPath = path.join("emails", "templates", "passwordResetEmail.html");
+  const resetEmailPath = path.join(
+    "emails",
+    "templates",
+    "passwordResetEmail.html"
+  );
   let htmlContent = fs.readFileSync(resetEmailPath, "utf8");
   return htmlContent.replace("{RESET_CODE}", resetCode);
-}
+};
 
+// SEND PASSWORD RESET EMAIL
 const sendPasswordResetEmail = async (email, resetCode) => {
   try {
-
     const htmlContent = getResetEmail(resetCode);
 
     const mailInfo = {
       from: sender,
       to: email,
       subject: "Reset Your Password",
-      html: htmlContent
+      html: htmlContent,
     };
 
     const sentResult = await transporter.sendMail(mailInfo);
-
+    console.log("email sent");
     return sentResult;
   } catch (error) {
     console.error(error);
   }
-}
+};
+
+// SEND TRACKING NUMBER EMAILS WHEN THE PARCEL IS REGISTERED
+
+const getTrackingNumberEmail = (parcelId, trackingNumber) => {
+  const trackingNumberEmailPath = path.join(
+    "emails",
+    "templates",
+    "trackingNumberEmail.html"
+  );
+  let htmlContent = fs.readFileSync(trackingNumberEmailPath, "utf8");
+  return htmlContent
+    .replace("{PARCEL_ID}", parcelId)
+    .replace("{TRACKING_NO}", trackingNumber);
+};
+
+const sendTrackingNumberEmail = async (email, parcelId, trackingNumber) => {
+  try {
+    const htmlContent = getTrackingNumberEmail(parcelId, trackingNumber);
+
+    const mailInfo = {
+      from: sender,
+      to: email,
+      subject: "Your Parcel Tracking Number",
+      html: htmlContent,
+    };
+    const sentResult = await transporter.sendMail(mailInfo);
+    console.log("email sent");
+    return sentResult;
+  } catch (error) {
+    console.error(error);
+  }
+};
 
 module.exports = {
   transporter,
-  sendPasswordResetEmail
+  sendPasswordResetEmail,
+  sendTrackingNumberEmail
 };
