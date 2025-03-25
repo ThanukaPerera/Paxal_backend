4a5d5275c55f4f2826f61513214ece0c93ee2aed
Kalani Chandrasena <kalanichandrasena99@gmail.com>
2025-03-25 14:42:13 +0530

    Update changes in other files

--- server.js
+++ server.js
@@ -1,28 +1,44 @@
-const express =require("express");
-const mongoose=require('mongoose');
-const bodyParser = require('body-parser');
-const routes=require('./routes/customer');
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const routes = require("./routes/customer");
+const staffRoutes = require("./routes/staffRoutes");
+const parcelRoutes = require("./routes/parcelRoutes");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
 
 const app = express();
-const PORT =8000;
+const PORT = 8000;
 
+app.use(
+  cors({
+    origin: "http://localhost:5173", // Your frontend URL
+    credentials: true, // Allow credentials (cookies)
+  })
+);
 
-app.use(bodyParser.json());
+app.use(express.json());
+app.use(cookieParser());
 app.use(routes);
 
 
+app.use("/staff", staffRoutes);
+app.use("/staff/parcel", parcelRoutes);
 
 
-const db_URL='mongodb+srv://AlgoRhythm-PAXAL:Sehara2002@pms.5jolo.mongodb.net/?retryWrites=true&w=majority&appName=PMS';
 
-mongoose.connect(db_URL)
-.then(()=>{
+
+// const db_URL='mongodb+srv://AlgoRhythm-PAXAL:Sehara2002@pms.5jolo.mongodb.net/?retryWrites=true&w=majority&appName=PMS';
+
+mongoose
+  .connect(db_URL)
+  .then(() => {
     console.log("Database connected successfully");
-})
-.catch((err)=>{
-    console.log("DB connection error",err)
-})
+  })
+  .catch((err) => {
+    console.log("DB connection error", err);
+  });
 
-app.listen(PORT,()=>{
-    console.log(`Server is running on ${PORT}`);
+app.listen(PORT, () => {
+  console.log(`Server is running on ${PORT}`);
 });
