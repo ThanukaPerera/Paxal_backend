d6e84944203e3fc47dda5a5bae6a5cde87c4097e
Kalani Chandrasena <kalanichandrasena99@gmail.com>
2025-04-23 23:42:58 +0530

    staff authentication updated

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -36,21 +36,17 @@ const authenticateAdmin = async(req, res, next) => {
 const authenticateStaff = async(req, res, next) => {
   try {
     const token = req.cookies.StaffToken; // Get token from cookies
-    console.log(token)
+   
     if (!token) {
       return res.status(401).json({ message: "Unauthorized: No token provided" });
     }
 
     // Verify token
-    const decoded =  jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
-      if (err) {
-        return res.status(403).json({ message: "Unauthorized: Invalid token" });
-      }
-      
+    const decoded =  jwt.verify(token, process.env.JWT_SECRET);
       req.staff = decoded; // Store staff data in request
       
       next();
-    });
+    ;
   } catch (error) {
     res.status(500).json({ message: "Authentication error", error });
   }
