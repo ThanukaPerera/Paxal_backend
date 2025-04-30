0a3ff52ae50656d2f9bb1a4313aca08c98efe383
Kalani Chandrasena <kalanichandrasena99@gmail.com>
2025-04-30 12:58:46 +0530

    deleted files and jsconfig update

--- routes/staff/vehicleScheduleRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { getAllPickupSchedulesForDate } = require("../../controllers/staff/vehicleScheduleController");
-const {authenticateStaff} = require("../../middlewares/authMiddleware");
-
-// GET ALL Pickup Schedules For a Given Day
-router.get('/get-all-pickup-schedules', getAllPickupSchedulesForDate);
-
-module.exports = router;
