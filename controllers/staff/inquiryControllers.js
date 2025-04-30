9164d84723684a009a3fa428a8de62a0e0a3eabe
Kalani Chandrasena <kalanichandrasena99@gmail.com>
2025-04-30 12:48:55 +0530

    updated inquiry controllers

--- controllers/staff/inquiryControllers.js
+++ controllers/staff/inquiryControllers.js
@@ -1,92 +1,129 @@
 const Inquiry = require("../../models/InquiryModel");
+const Staff = require("../../models/StaffModel");
+const Parcel = require("../../models/ParcelModel");
+const { sendInquiryReplyEmail } = require("../../emails/emails");
 
+// retrieve all replied inquiries
+const getRepliedInquiries = async (req, res) => {
+  try {
+    // Find the branch that requests the inquiries.
+    const staff_id = req.staff._id.toString();
+    const staff = await Staff.findById(staff_id);
+    const branch_id = staff.branchId;
 
-// //SEND INQUIRY BY CUSTOMER
-// const sendInquiry = async(req,res) => {
-//     try {
-//         // find last inquiry ID and generate the next one
-//         const lastInquiry = await Inquiry.findOne().sort({inquiryId: -1}).lean();
-//         let nextInquiryId = "INQUIRY001"; // default inquiry ID if no inquiry exists
+    // Find all parcels registered to the branch and get the tracking number.
+    const parcels = await Parcel.find({ from: branch_id }).select("trackingNo").lean();
+    const trackingNumbers = parcels.map((parcel) => parcel.trackingNo);
 
-//         if (lastInquiry) {
-//             const lastIdNumber = parseInt(
-//                 lastInquiry.inquiryId.replace("INQUIRY",""),10
-//             );
-//             nextInquiryId = `INQUIRY${String(lastIdNumber + 1).padStart(3, "0")}`;
-//         }
+    // Find inquiries that belong to the branch (matching those tracking numbers).
+    const inquiries = await Inquiry.find({
+      parcelTrackingNo: { $in: trackingNumbers },
+      status: "solved",
+    });
 
-//         // find the customer object ID
-//         const customer = await Customer.findOne({customerId: req.params.customer});
-//         const customerRef = customer._id;
+    return res.status(200).json(inquiries);
+  } catch (error) {
+    return res.status(500).json({ message: "Error fetching inquiries", error });
+  }
+};
 
-//         // create a new inquiry with the new inquiry ID
-//         const inquiryData = {
-//             ...req.body,
-//             inquiryId: nextInquiryId,
-//             status: "not",
-//             customerId: customerRef
-//         }
+//get new inquiries received to the branch
+const getAllNewInquiries = async (req, res) => {
+  try {
+    // FInd the branch that requests the inquiries.
+    const staff_id = req.staff._id.toString();
+    const staff = await Staff.findById(staff_id);
+    const branch_id = staff.branchId;
+   
+    // Find all parcels registered to the branch and get the tracking number.
+    const parcels = await Parcel.find({ from: branch_id }).select("trackingNo").lean();    
+    const trackingNumbers = parcels.map((parcel) => parcel.trackingNo);
 
-//         const inquiry = new Inquiry(inquiryData);
-//         const sentInquiry = await inquiry.save();
+    // Find inquiries that belong to the branch (matching those tracking numbers).
+    const inquiries = await Inquiry.find({
+      parcelTrackingNo: { $in: trackingNumbers },
+      status: "new",
+    });
 
-//         res.status(200).json({message:"Inquiry is sent", sentInquiry})
-//     } catch (error) {
-//         res.status(500).json({message:"Error sending inquiry", error});
-//     }
-// }
+    return res.status(200).json(inquiries);
+    
+  } catch (error) {
+    return res.status(500).json({ message: "Error fetching inquiries", error });
+  }
+};
 
-// GET ALL INQUIRIES
-const getAllInquiries = async(req, res) => {
+// get one new inquiry details by inquiryId
+const getOneInquiry = async (req, res) => {
+  try {
+    const inquiry = await Inquiry.findOne({ inquiryId: req.params.inquiryId });
 
-    try {
-        const inquiries = await Inquiry.find().sort({createdAt: -1})
-        res.status(200).json(inquiries);
-    } catch (error) {
-        res.status(500).json({message:"Error fetching inquiries", error});
+    if (!inquiry) {
+      return res.status(404).json({ message: "Inquiry not found" });
     }
 
-}
+    return res.status(200).json(inquiry);
+  } catch (error) {
+    return res.status(500).json({ message: "Error fetching the inquiry", error });
+  }
+};
 
-// GET ONE INQUIRY
-const getOneInquiry = async(req,res) => {
-    try {
-        const inquiry = await Inquiry.findOne({inquiryId: req.params.inquiryId});
-        if (!inquiry) {
-            return res.status(404).json({message: "Inquiry not found"});
-        }
-        res.status(200).json(inquiry);
-    } catch (error) {
-        res.status(500).json({message:"Error fetching the inquiry", error});
-        
+// get one replied inquiry details by inquiryId
+const getOneRepliedInquiry = async (req, res) => {
+  try {
+    const inquiry = await Inquiry.findOne({ inquiryId: req.params.inquiryId }).populate("staffId", "name staffId");
+
+    if (!inquiry) {
+      return res.status(404).json({ message: "Inquiry not found" });
     }
-}
 
-// SEND REPLY BY STAFF
-const sendReply = async(req,res) => {
-    try {
-        const replyData = {
-            ...req.body,
-            status: "solved"
-        }
+    return res.status(200).json(inquiry);
+  } catch (error) {
+    return res.status(500).json({ message: "Error fetching the inquiry", error });
+  }
+};
 
-        const filter = {inquiryId: req.params.inquiryId}
-        const inquiry = await Inquiry.findOneAndUpdate(filter, replyData, {new:true});
 
-        // send the email
-        const {name, email, parcelTrackingNo} = await Inquiry.findOne({inquiryId});
+// send a reply to an inquiry
+const replyToInquiry = async (req, res) => {
+  try {
 
-        res.status(200).json("Reply is sent", inquiry);
-    } catch (error) {
-        res.status(500).json({message:"Error sending reply", error});
+    // Find the staff who replied to the inquiry.
+    const staff_id = req.staff._id.toString();
+
+    const inquiryId = req.params.inquiryId;
+
+    const replyData = {
+      ...req.body,
+      status: "solved",
+      staffId: staff_id,
+    };
+
+    // Update the inquiry.
+    const filter = { inquiryId: inquiryId };
+    const inquiry = await Inquiry.findOneAndUpdate(filter, replyData, {new: true,});
+
+    // Find the details of the inquiry to be sent in the email.
+    const updatedInquiry = await Inquiry.findOne({ inquiryId: inquiryId }).populate("staffId", "name");;
+    const { parcelTrackingNo, name, email, createdAt, message, reply} = updatedInquiry;
+    const staffName = updatedInquiry.staffId.name;
+
+    // Send the email.
+    const result = await sendInquiryReplyEmail(email, parcelTrackingNo, name, reply,createdAt, message, staffName);
+    if(!result.success) {
+      console.log("Error in sending the inquiry reply email",result)
     }
-}
+      
+    return res.status(200).json({ message: "Inquiry reply sent successfully"});
 
+  } catch (error) {
+    return res.status(500).json({ message: "Error sending reply", error });
+  }
+};
 
 module.exports = {
-
-    getAllInquiries,
-    getOneInquiry,
-    sendReply,
-
-}
+  getRepliedInquiries,
+  getAllNewInquiries,
+  getOneInquiry,
+  getOneRepliedInquiry,
+  replyToInquiry
+};
