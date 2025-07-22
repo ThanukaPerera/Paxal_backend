26b33ae307586bc9d7b8844f8b5b1cd0c7198e81
ThanukaPerera <>
2025-07-22 09:22:29 +0530

    Revert "feat(auth): Update cookie settings for admin login and improve security"
    This reverts commit dfaeddd662839fef2734cc2ea9b7e1a5d83b7831.

--- controllers/adminControllers/authentication/adminLogin.js
+++ controllers/adminControllers/authentication/adminLogin.js
@@ -60,20 +60,18 @@ const adminLogin = async (req, res) => {
       { expiresIn: REFRESH_TOKEN_EXPIRY }
     );
 
-
-    const isProduction = process.env.NODE_ENV === 'production';
     // Set cookies
     res.cookie('AdminToken', accessToken, { 
       httpOnly: true, 
-      secure: true, 
-      sameSite: 'none', 
+      secure: process.env.NODE_ENV === 'production', 
+      sameSite: 'strict', 
       maxAge: ACCESS_TOKEN_COOKIE_MAX_AGE
     });
     
     res.cookie('AdminRefreshToken', refreshToken, { 
       httpOnly: true, 
-      secure: true, 
-      sameSite: 'none', 
+      secure: process.env.NODE_ENV === 'production', 
+      sameSite: 'strict', 
       maxAge: REFRESH_TOKEN_COOKIE_MAX_AGE
     });
 
