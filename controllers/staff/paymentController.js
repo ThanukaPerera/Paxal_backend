34fd3882c54641120347bd5a5ca0002d3b94f9cc
Kalani Chandrasena <kalanichandrasena99@gmail.com>
2025-07-25 18:51:51 +0530

    updated payment,receiver models and controllers

--- controllers/staff/paymentController.js
+++ controllers/staff/paymentController.js
@@ -63,14 +63,14 @@ const calculatePayment = async(req, res) => {
 const savePayment = async (paymentData, session) => {
   try {
 
-    // Find last payment ID and generate the next one.
-    const lastPayment = await Payment.findOne().sort({ paymentId: -1 }).lean();
-    let nextPaymentId = "PAYMENT001"; // Default ID if no payment exists.
+    // // Find last payment ID and generate the next one.
+    // const lastPayment = await Payment.findOne().sort({ paymentId: -1 }).lean();
+    // let nextPaymentId = "PAYMENT001"; // Default ID if no payment exists.
 
-    if (lastPayment) {
-      const lastIdNumber = parseInt(lastPayment.paymentId.replace("PAYMENT", ""),10);
-      nextPaymentId = `PAYMENT${String(lastIdNumber + 1).padStart(3, "0")}`;
-    }
+    // if (lastPayment) {
+    //   const lastIdNumber = parseInt(lastPayment.paymentId.replace("PAYMENT", ""),10);
+    //   nextPaymentId = `PAYMENT${String(lastIdNumber + 1).padStart(3, "0")}`;
+    // }
 
     const {paymentMethod} = paymentData;
     let paidBy, paymentStatus, paymentDate;
@@ -90,7 +90,7 @@ const savePayment = async (paymentData, session) => {
     // create the payment.
     const newPayment = {
       ...paymentData,
-      paymentId: nextPaymentId,
+      // paymentId: nextPaymentId,
       paidBy,
       paymentStatus,
       paymentDate,
