ab9afc551b7a3a24f0cbaeb18123feac96c84209
DRKBihimuthu <dbihimuthu@gmail.com>
2025-07-29 17:49:53 +0530

    deeraka main changes

--- routes/paymentRoutes.js
+++ routes/paymentRoutes.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const { getCheckoutSession } = require("../controllers/paymentController");
+const calculatePayment = require("../controllers/priceCalc");
+
 
 const router = express.Router();
 
 router.post("/checkout", getCheckoutSession);
+router.get("/get-price",calculatePayment)
 
 module.exports = router;
